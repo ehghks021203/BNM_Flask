be68239d6d4e4b114b12455ef69ce80d9867434c
ehghks021203 <ehghks021203@gmail.com>
2023-12-15 01:46:34 +0900

    modify run.py

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 from app import app
 from config import BASEDIR, Server
-import ssl
 
 if __name__ == "__main__":
-    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-    ssl_context.load_cert_chain(certfile=BASEDIR+"/SSL/fullchain.pem", keyfile=BASEDIR+"/SSL/privkey.pem", password="csserver8403598*")
-    app.run(host=Server.SERVER_DOMAIN, port=Server.SERVER_PORT, ssl_context=ssl_context, debug=True)
+    app.run(host=Server.SERVER_DOMAIN, port=Server.SERVER_PORT, ssl_context=Server.SSL_CONTEXT, debug=True)
